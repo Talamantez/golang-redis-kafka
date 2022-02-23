b072bdfb3c8b22117af73c436c5cebb51bdd3dd3
Talamantez <robert.m.talamantez@gmail.com>
2022-02-23 20:09:10 +0000

    resolve and remove noodlog

--- main.go
+++ main.go
@@ -7,57 +7,24 @@ import (
 	"video-feed/redis"
 
 	"github.com/gorilla/mux"
-	"github.com/gyozatech/noodlog"
 	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
-var log *noodlog.Logger
-
-func init() {
-	log = noodlog.NewLogger().SetConfigs(
-		noodlog.Configs{
-			LogLevel:             noodlog.LevelTrace,
-			JSONPrettyPrint:      noodlog.Enable,
-			TraceCaller:          noodlog.Enable,
-			Colors:               noodlog.Enable,
-			CustomColors:         &noodlog.CustomColors{Trace: noodlog.Cyan},
-			ObscureSensitiveData: noodlog.Enable,
-			SensitiveParams:      []string{"password"},
-		},
-	)
-}
-
 func launchServer() error {
-
-	err := http.ListenAndServe(":4014", nil)
+	err := http.ListenAndServe(":4015", nil)
 	if err != nil {
 		fmt.Printf("Server error %v :", err)
 	}
-
 	return nil
 }
 
 func main() {
-	// simple string message (with custom color)
-	log.Trace("Hello world!")
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 
-	// chaining elements
-	log.Info("You've reached", 3, "login attemps")
+	log.Info().Msg("Running App")
+	log.Info().Msg("Localhost Endpoint: {your port}/produce-to-incoming-topic?message={your message}")
 
-	// using string formatting
-	log.Warn("You have %d attempts left", 2)
-
-	// logging a struct with a JSON
-	log.Error(struct {
-		Code  int
-		Error string
-	}{500, "Generic Error"})
-
-	// logging a raw JSON string with a JSON (with obscuring "password")
-	log.Info(`{"username": "gyozatech", "password": "Gy0zApAssw0rd"}`)
-
-	// logging a JSON string with a JSON (with obscuring "password")
-	log.Info("{\"username\": \"nooduser\", \"password\": \"N0oDPasSw0rD\"}")
 	router := mux.NewRouter()
 
 	router.HandleFunc("/produce-to-incoming-topic", ProduceToIncomingTopic)
@@ -67,7 +34,7 @@ func main() {
 	redis.Init()
 	go kafka.InitProducer()
 	go kafka.Consumer([]string{"InboundTopic", "OutboundTopic"})
-	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 
 	launchServer()
+
 }
