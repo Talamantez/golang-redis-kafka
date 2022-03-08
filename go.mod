6f18df6dcfc7683b30503128d36e390c07c0ab0b
vagrant <vagrant@vagrant.vm>
2022-03-08 07:33:18 +0000

    init write to json file

--- go.mod
+++ go.mod
@@ -8,4 +8,5 @@ require (
 	github.com/gorilla/mux v1.8.0
 	github.com/joho/godotenv v1.4.0
 	github.com/rs/zerolog v1.26.1
+	github.com/sirupsen/logrus v1.8.1 // indirect
 )
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"main/kafka"
 	"main/redis"
 	"net/http"
@@ -11,6 +10,7 @@ import (
 	"github.com/joho/godotenv"
 
 	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
 )
 
 func launchServer() error {
@@ -21,44 +21,33 @@ func launchServer() error {
 	return nil
 }
 
-var (
-	WarningLogger *log.Logger
-	InfoLogger    *log.Logger
-	ErrorLogger   *log.Logger
-)
-
 func init() {
-	file, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	InfoLogger = log.New(file, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
-	WarningLogger = log.New(file, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
-	ErrorLogger = log.New(file, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+	// file, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 }
 
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
-		ErrorLogger.Println("")
 		os.Exit(1)
 	}
+	log.SetFormatter(&log.JSONFormatter{})
+
 	// If the file doesn't exist, create it or append to the file
-	file, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	file, err := os.OpenFile("logs.json", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	log.SetOutput(file)
-	log.Println("**** LOG ****")
-
-	InfoLogger.Println("Starting the application...")
-	InfoLogger.Println("Something noteworthy happened")
-	WarningLogger.Println("There is something you should know about")
-	ErrorLogger.Println("Something went wrong")
-
-	InfoLogger.Println("Localhost Endpoint: {your port}/produce-to-incoming-topic?message={your message}")
+	// log.Println("**** LOG ****")
+	log.Debug("Useful debugging information.")
+	log.Info("Something noteworthy happened!")
+	log.Warn("You should probably take a look at this.")
+	log.Error("Something failed but I'm not quitting.")
+	// log.Info("Localhost Endpoint: {your port}/produce-to-incoming-topic?message={your message}")
 	router := mux.NewRouter()
 
 	router.HandleFunc("/produce-to-incoming-topic", ProduceToIncomingTopic)
